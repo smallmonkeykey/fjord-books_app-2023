5584322643491b4d8864657eada52538666dc417
smallmonkeykey <sarinitakuoo@gmail.com>
2023-10-19 15:38:08 +0900

    ログアウト機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resource)
     books_path(resource)
   end
+
+  def after_sign_out_path_for(resource)
+    new_user_registration_path(resource)
+  end
 end
