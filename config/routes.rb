cedf93425f6b0d8f1c6f08a2242b5208750ac335
smallmonkeykey <sarinitakuoo@gmail.com>
2023-10-20 15:45:32 +0900

    アカウント編集ページとユーザーの一覧ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # get "users" => "users#index"
+  # get 'users/:id/edit' => "users#edit"
   # Defines the root path route ("/")
   # root "articles#index"
 end
