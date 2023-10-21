7a1efcc37aaca40ce02636de3d68602246a68e26
smallmonkeykey <sarinitakuoo@gmail.com>
2023-10-21 16:38:57 +0900

    属性名を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:zipcode, :address, :introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:zip, :address, :introduction])
   end
 
 	def after_sign_in_path_for(resource)
