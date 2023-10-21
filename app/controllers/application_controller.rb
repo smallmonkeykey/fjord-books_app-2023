0f41a343bd363fe692580cb4b8a95a215f24a16f
smallmonkeykey <sarinitakuoo@gmail.com>
2023-10-21 16:33:26 +0900

    ユーザーの一覧ページ作成のためのフォームに新しい属性を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
 	def after_sign_in_path_for(resource)
     books_path(resource)
   end
