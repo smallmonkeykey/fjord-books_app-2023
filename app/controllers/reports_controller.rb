f0400b6aa153cfdc242468764a8fe466163c68e7
smallmonkeykey <sarinitakuoo@gmail.com>
2023-11-07 14:27:20 +0900

    reportモデルにデータを保存する

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,14 +4,29 @@ class ReportsController < ApplicationController
     @reports = Report.order(:id).page(params[:page])
   end
 
-	# def new
-  #   @report = Report.new
-  # end
+	def new
+    @report = Report.new
+  end
 
-	  def create
-    	report = Report.new(report_params)
-			task.save!
-			redirect_to reports_url
+	def create
+    	# @report = Report.new(report_params).users.build
+			# @report.save
+
+    @user = current_user
+    @report = @user.reports.create(report_params)
+
+    if @report.save
+      puts "保存した" 
+    else
+      puts "保存できない" 
+      p report_params
+    end
+
+    redirect_to reports_path
+
+    #   @report = Report.create!(report_params)
+		# redirect_to reports_path
+
     # respond_to do |format|
     #   if @report.save
     #     format.html { redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: report.model_name.human) }
@@ -20,6 +35,7 @@ class ReportsController < ApplicationController
     #     format.html { render :new, status: :unprocessable_entity }
     #     format.json { render json: @report.errors, status: :unprocessable_entity }
     #   end
+		# end
   end
 
 	private
