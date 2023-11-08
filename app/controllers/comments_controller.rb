a63459828cc2f12afad46041cfb319bbe9f51684
smallmonkeykey <sarinitakuoo@gmail.com>
2023-11-08 17:19:05 +0900

    commentモデルに保存

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,21 @@
 class CommentsController < ApplicationController
 
-    def new
-        @report = Report.find(params[:report_id])
-    end
+	def new
+    @report = Report.find(params[:report_id])
+    @comment = Comment.new
+	end
+
+	def create
+    @report = Report.find(params[:report_id])
+		@comment = @report.comments.create(comment_params)
+    @comment.user = current_user
+
+    redirect_to report_path(@report)
+  end
+
+  private
+	def comment_params
+    params.require(:comment).permit(:content)
+  end
+
 end
