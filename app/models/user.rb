b762a62ff49c62f1f16c250aee1b48d526d7d527
smallmonkeykey <sarinitakuoo@gmail.com>
2023-11-14 13:40:28 +0900

    郵便番号のバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :zip, numericality: { only_integer: true }, length: { is: 7 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_21_080223) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_14_042901) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -28,7 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_21_080223) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "zip"
+    t.text "zip"
     t.text "address"
     t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
