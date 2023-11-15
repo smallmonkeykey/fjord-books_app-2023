1c037c031c47c7c410adcc4fd8d7f36c9617d25a
smallmonkeykey <sarinitakuoo@gmail.com>
2023-11-15 18:38:33 +0900

    不要なコメントを削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,5 @@
 class CommentsController < ApplicationController
 
-	# def new
-  #   @report = Report.find(params[:report_id])
-  #   @comment = Comment.new
-	# end
-
-	# def create
-  #   # @report = Report.find(params[:report_id])
-	# 	# @comment = @report.comments.create(comment_params)
-  #   # @comment.user = current_user
-
-	# @comment = @commentable.comments.new(comment_params)
-	# @comment.user = current_user
-	# @comment.save
-
-  #  if @comment.save
-  #      redirect_to report_path(@report)
-  #  end
-
   def create
     @comment = @commentable.comments.build(comment_params)
     @comment.user = current_user
