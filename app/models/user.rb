c5f9c8ead7c47fa27406c4fdf5b343cafbd03d8e
smallmonkeykey <sarinitakuoo@gmail.com>
2023-11-18 12:46:48 +0900

    空白でも可を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :zip, numericality: { only_integer: true }, length: { is: 7 }
+  validates :zip, numericality: { only_integer: true }, length: { is: 7 }, allow_blank: true
 end
