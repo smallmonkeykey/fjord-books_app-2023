12baf8502efec28c473f02a2229f303caf901a4d
smallmonkeykey <sarinitakuoo@gmail.com>
2023-11-19 14:59:52 +0900

    indexを追加

--- db/migrate/20231119055507_create_mentions.rb
+++ db/migrate/20231119055507_create_mentions.rb
@@ -6,5 +6,8 @@ class CreateMentions < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+    add_index :mentions, :mentioning_id
+    add_index :mentions, :mentioned_id
+    add_index :mentions, [:mentioning_id, :mentioned_id], unique: true
   end
 end
