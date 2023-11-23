981e75d255a7a05fb164c916023d2c39e4e9b4dc
smallmonkeykey <sarinitakuoo@gmail.com>
2023-11-23 11:24:55 +0900

    update前後で値が同じ時の処理を追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -37,21 +37,21 @@ class ReportsController < ApplicationController
 
   def update
     if @report.update(report_params)
-      matches = @report.content.scan(/http:\/\/localhost:3000\/reports\/(\d+)/)
+      new_data = @report.content.scan(/http:\/\/localhost:3000\/reports\/(\d+)/).flatten.sort
+      old_data = @report.mentioning_reports.all.pluck(:mentioned_id).sort
 
-      @report.mentioning_reports.destroy_all
+      unless old_data == new_data
+         @report.mentioning_reports.destroy_all
+         @report = current_user.reports.new(report_params)
 
-      @report = current_user.reports.new(report_params)
+          if @report.save
+             matches = @report.content.scan(/http:\/\/localhost:3000\/reports\/(\d+)/)
 
-      if @report.save
-      matches = @report.content.scan(/http:\/\/localhost:3000\/reports\/(\d+)/)
-
-      matches.each do |match|
-        Mention.create(mentioning_id: @report.id, mentioned_id: match[0].to_i)
-      end
+             matches.each do |match|
+              Mention.create(mentioning_id: @report.id, mentioned_id: match[0].to_i)
+             end
+          end
     end
-
-      binding.irb 
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
