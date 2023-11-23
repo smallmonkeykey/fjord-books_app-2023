b613d8665f22e394e91ce2b05d72ea5b9fe1c707
smallmonkeykey <sarinitakuoo@gmail.com>
2023-11-23 11:47:49 +0900

    reportsテーブルだけupdateするように変更

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -42,16 +42,13 @@ class ReportsController < ApplicationController
 
       unless old_data == new_data
          @report.mentioning_reports.destroy_all
-         @report = current_user.reports.new(report_params)
 
-          if @report.save
-             matches = @report.content.scan(/http:\/\/localhost:3000\/reports\/(\d+)/)
+         matches = @report.content.scan(/http:\/\/localhost:3000\/reports\/(\d+)/)
 
-             matches.each do |match|
-              Mention.create(mentioning_id: @report.id, mentioned_id: match[0].to_i)
-             end
-          end
-    end
+         matches.each do |match|
+            Mention.create(mentioning_id: @report.id, mentioned_id: match[0].to_i)
+         end
+      end
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
