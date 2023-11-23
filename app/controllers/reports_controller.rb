c8df3bb8995f6da64a5c816ec9660bb735d7fc53
smallmonkeykey <sarinitakuoo@gmail.com>
2023-11-23 10:51:29 +0900

    update処理を追加

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -37,6 +37,21 @@ class ReportsController < ApplicationController
 
   def update
     if @report.update(report_params)
+      matches = @report.content.scan(/http:\/\/localhost:3000\/reports\/(\d+)/)
+
+      @report.mentioning_reports.destroy_all
+
+      @report = current_user.reports.new(report_params)
+
+      if @report.save
+      matches = @report.content.scan(/http:\/\/localhost:3000\/reports\/(\d+)/)
+
+      matches.each do |match|
+        Mention.create(mentioning_id: @report.id, mentioned_id: match[0].to_i)
+      end
+    end
+
+      binding.irb 
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
