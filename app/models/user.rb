e4874580dae0821f86616566bf9656857252e944
smallmonkeykey <sarinitakuoo@gmail.com>
2023-11-27 22:15:32 +0900

    コメントの投稿者名を表示するメソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [150, 150]
   end
+
+  def display_name
+    name.empty? ? email : name
+  end
 end
