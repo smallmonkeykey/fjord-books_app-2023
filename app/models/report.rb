a3d324063886f434b0c904958ca387579b60c411
smallmonkeykey <sarinitakuoo@gmail.com>
2023-11-27 10:06:39 +0900

    inverse_ofを削除

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Report < ApplicationRecord
-  belongs_to :users, class_name: 'User', foreign_key: 'user_id', inverse_of: :user
+  belongs_to :users, class_name: 'User', foreign_key: 'user_id'
   has_many :comments, as: :commentable, dependent: :destroy
 end
