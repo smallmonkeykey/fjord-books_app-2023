54027ae9f60fe44adda64ee3ef25ebd3829dacdb
smallmonkeykey <sarinitakuoo@gmail.com>
2023-11-30 13:47:24 +0900

    reject_invalid_user名に変更

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,7 @@
 
 class ReportsController < ApplicationController
   before_action :set_report, only: %i[show edit update destroy]
-  before_action :correct_user, only: %i[edit update destroy]
+  before_action :reject_invalid_user, only: %i[edit update destroy]
 
   def index
     @reports = Report.order(:id).page(params[:page])
@@ -52,7 +52,7 @@ class ReportsController < ApplicationController
     params.require(:report).permit(:title, :content)
   end
 
-  def correct_user
+  def reject_invalid_user
     redirect_to(root_url) if @report.user_id != current_user.id
   end
 end
