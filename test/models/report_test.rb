9c4c283f7d392f7d072c06a17b12b0837310a3a2
smallmonkeykey <sarinitakuoo@gmail.com>
2023-12-10 19:05:17 +0900

    editable?(target_user)のテスト

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,12 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#editable?" do
+    user = User.create!(email: 'a@example.com', password: 'password')
+    user2 = User.create!(email: 'b@example.com', password: 'password')
+    report = user.reports.create!(title: 'title', content: 'content')
+
+    assert (report.user == user )
+    assert_not (report.user == user2)
+  end
 end
