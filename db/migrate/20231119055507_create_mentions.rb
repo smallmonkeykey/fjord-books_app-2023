7472fc7ca1a7f2356b6cb9b907afea63a6fcaa67
smallmonkeykey <sarinitakuoo@gmail.com>
2023-12-12 18:16:46 +0900

    references の箇所を短縮

--- db/migrate/20231119055507_create_mentions.rb
+++ db/migrate/20231119055507_create_mentions.rb
@@ -1,8 +1,7 @@
 class CreateMentions < ActiveRecord::Migration[7.0]
   def change
     create_table :mentions do |t|
-      t.references :mentioning, null: false, foreign_key: {to_table: :reports}
-      t.references :mentioned, null: false, foreign_key: {to_table: :reports}
+      t.references :mentioning, :mentioned, null: false, foreign_key: {to_table: :reports}
       t.timestamps
     end
     add_index :mentions, [:mentioning_id, :mentioned_id], unique: true
