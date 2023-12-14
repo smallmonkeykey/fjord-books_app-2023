db689cbf03afe1468c78da4f34dca9314aec22a8
smallmonkeykey <sarinitakuoo@gmail.com>
2023-12-14 18:10:18 +0900

    referencesの箇所を変更

--- db/migrate/20231119055507_create_mentions.rb
+++ db/migrate/20231119055507_create_mentions.rb
@@ -1,7 +1,8 @@
 class CreateMentions < ActiveRecord::Migration[7.0]
   def change
     create_table :mentions do |t|
-      t.references :mentioning, :mentioned, null: false, foreign_key: {to_table: :reports}
+      t.references :mentioning, null: false, foreign_key: {to_table: :reports}
+      t.references :mentioned, null: false, foreign_key: {to_table: :reports}
       t.timestamps
     end
     add_index :mentions, [:mentioning_id, :mentioned_id], unique: true
